b77b80f183f81323db1c98559c70af950bc4ccab
thefotes <peterfoti@me.com>
2014-09-20 19:59:14 -0400

    Add instagram model

---
+++ Instagram To Evernote/PFInstagramObject.h
@@ -0,0 +1,13 @@
+//
+//  PFInstagramObject.h
+//  Instagram To Evernote
+//
+//  Created by Peter Foti on 9/20/14.
+//  Copyright (c) 2014 Peter Foti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PFInstagramObject : NSObject
+
+@end
