befa111ec395231e48e38c2b71bde703340cf805
thefotes <peterfoti@me.com>
2014-09-20 21:28:48 -0400

    Add instagram parser to project

---
+++ Instagram To Evernote/PFInstagramJSONParser.h
@@ -0,0 +1,13 @@
+//
+//  PFInstagramJSONParser.h
+//  Instagram To Evernote
+//
+//  Created by Peter Foti on 9/20/14.
+//  Copyright (c) 2014 Peter Foti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PFInstagramJSONParser : NSObject
+
+@end
